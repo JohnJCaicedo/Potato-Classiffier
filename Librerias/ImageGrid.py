131935bd8affe290717467b4e932e22fb9e7e7b2
JohnJCaicedo <82515148+JohnJCaicedo@users.noreply.github.com>
2021-10-13 12:25:30 -0500

    More fixes to ImageGrid

--- Librerias/ImageGrid.py
+++ Librerias/ImageGrid.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 
 def class_switch(argument):
+    # Classes 
+    classes = ('Clase 1', 'Clase 2', 'Clase 3', 'Clase 4' ,'Clase 5' , 'Clase 6',
+               'Clase 7', 'Clase 8', 'Clase 9', 'Clase 10','Clase 11', 'Clase 12')
     switcher = {
         1: classes[0],
         2: classes[1],
